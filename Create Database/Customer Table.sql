ad243d6bf8a25008313c4eb40a491b2b661b72d6
Austin-Caldwell <austin.caldwell295@gmail.com>
2015-10-15 16:08:43 -0400

    Tables Added to Database
    SQL Commands to add four different tables to the YellowBucketCSC365
    database: Customer, CustomerAddress, Movie, and RatingReview

---
+++ Create Database/Customer Table.sql
@@ -0,0 +1,13 @@
+USE YellowBucketCSC365
+GO
+
+CREATE TABLE Customer -- Create a table of customers
+(
+	customerID int PRIMARY KEY IDENTITY(0000,1) NOT NULL,
+	name varchar(32) NOT NULL,
+	email varchar(32) NOT NULL,
+	userName varchar(32) NOT NULL,
+	userPassword varchar(24) NOT NULL,
+	creditCard int CONSTRAINT creditCard_LengthCC CHECK(creditCard = 16) NOT NULL,
+	customerAddressID int CONSTRAINT fk_Customer_CustomerAddress FOREIGN KEY REFERENCES CustomerAddress(customerAddressID) ON DELETE SET NULL
+);
---
+++ Create Database/RatingReview Table.sql
@@ -0,0 +1,12 @@
+USE YellowBucketCSC365
+GO
+
+CREATE TABLE RatingReview -- Create a table of ratings and review of movies
+(
+	reviewID int PRIMARY KEY IDENTITY(0000,1) NOT NULL,
+	datePosted date NOT NULL,
+	reviewDescription varchar(256) NULL,
+	rating int CONSTRAINT rating_Default DEFAULT 0 CONSTRAINT rating_CC CHECK(rating >= 0 AND rating <= 5) NOT NULL,
+	customerID int CONSTRAINT fk_RatingReview_Customer FOREIGN KEY REFERENCES Customer(customerID) ON DELETE CASCADE NOT NULL,
+	movieID int CONSTRAINT fk_RatingReview_Movie FOREIGN KEY REFERENCES Movies(movieID) ON DELETE CASCADE NOT NULL,
+);
