0dd6f1165e73c25669743db4dfdee96d29a4fd73
Austin-Caldwell <austin.caldwell295@gmail.com>
2015-10-22 19:15:35 -0400

    Changed Ratings and Changed avgRating Type
    Changed Ratings for a few sample movie reviews and movie entries.  Also
    changed avgRating type in Movie Table from int to decimal(2,1).

--- Create Database/Create Database and Tables.sql
+++ Create Database/Create Database and Tables.sql
@@ -1,5 +1,5 @@
 /* CREATE THE ENTIRE YellowBucketCSC365 DATABASE, AND CREATE/LINK ALL TABLES */ /* Austin Caldwell and Evan Wehr */
--- Updated 10/21/2015
+-- Updated 10/22/2015
 
 -- Create the database itself
 Use master
@@ -21,7 +21,7 @@ CREATE TABLE Movie -- Create a table of movies
 	movieID int PRIMARY KEY IDENTITY(0000,1) NOT NULL,
 	title varchar(64) NOT NULL,
 	movieDescription varchar(256) NOT NULL,
-	avgRating int CONSTRAINT avgRating_Default DEFAULT 0 CONSTRAINT avgRating_CC CHECK(avgRating >= 0 AND avgRating <=5) NOT NULL,
+	avgRating decimal(2,1) CONSTRAINT avgRating_Default DEFAULT 0 CONSTRAINT avgRating_CC CHECK(avgRating >= 0 AND avgRating <=5) NOT NULL,
 	director varchar(32) NOT NULL,
 	studio varchar(32) NOT NULL,
 	runTime int CONSTRAINT runTime_CC Check(runTime >= 0) NOT NULL,
--- Seed Database/Seed All Sample Database Tables.sql
+++ Seed Database/Seed All Sample Database Tables.sql
@@ -1,5 +1,5 @@
 /* SEED THE YellowBucketCSC365 DATABASE WITH SAMPLE DATA */ /* Austin Caldwell and Evan Wehr */
--- Updated 10/21/2015
+-- Updated 10/22/2015
 
 USE YellowBucketCSC365
 GO
@@ -48,9 +48,9 @@ INSERT INTO Movie(title, movieDescription, avgRating, director, studio, runTime,
 VALUES
 ('Star Wars', 'A tale of intergalactic war and the discovery of family lineage.', 5, 'George Lucas', 'Lucasfilm Studios', 125, 'PG', 'Sci-Fi', '10/10/1978'),
 ('The Lord of The Rings', 'Running, running,... more running. Action.', 5, 'Steven Wier', 'Gateway Films', 160, 'PG-13', 'Fantasy', '02/26/2002'),
-('The Prince and Me', 'Fall in love. Break up. Fall in love.', 4, 'Amberly Kissinger', 'Brighthouse', 130, 'PG-13', 'Romance', '08/21/2005'),
-('The Peanuts Movie', 'A lonely boy and his search for friends.', 4, 'Charles Schultz', 'ComicsRUs', 90, 'G', 'Kids', '06/15/1995'),
-('Penguins: A Documentary', 'Life of the tuxedo animals in Antarctica.', 3, 'Abigail Davis', 'Geneva Studios', 95, 'NR', 'Documentary', '12/13/2015');
+('The Prince and Me', 'Fall in love. Break up. Fall in love.', 2, 'Amberly Kissinger', 'Brighthouse', 130, 'PG-13', 'Romance', '08/21/2005'),
+('The Peanuts Movie', 'A lonely boy and his search for friends.', 0, 'Charles Schultz', 'ComicsRUs', 90, 'G', 'Kids', '06/15/1995'),
+('Penguins: A Documentary', 'Life of the tuxedo animals in Antarctica.', 2.5, 'Abigail Davis', 'Geneva Studios', 95, 'NR', 'Documentary', '12/13/2015');
 
 
 INSERT INTO Inventory(dvdBluRay, movieID, kioskID, quantityAtKiosk)
