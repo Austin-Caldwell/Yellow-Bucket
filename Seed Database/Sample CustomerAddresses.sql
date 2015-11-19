7c3af927918d86c274b71d58919528c7d7a56c62
Austin-Caldwell <austin.caldwell295@gmail.com>
2015-11-19 11:05:01 -0500

    Revert "Revert "Changes to Sample Customer Addresses""
    This reverts commit b77cc31e8047fca5dbf5a53f81ea35ea896ca51e.

--- Seed Database/Sample CustomerAddresses.sql
+++ Seed Database/Sample CustomerAddresses.sql
@@ -1,17 +1,17 @@
 USE YellowBucketCSC365
 GO
 
-INSERT INTO CustomerAddress(addressLine1, city, stateProvince, postalCode)
+INSERT INTO CustomerAddress(addressLine1, addressLine2, city, stateProvince, postalCode)
 VALUES
-('777 Kindgom Way', 'Ararat', 'New York', '13011'),
-('111 Lonestar Street', 'Mainville', 'Ohio', '18292'),
-('581 Maisly Ave.', 'Gateway', 'Pennsylvania', '16911'),
-('196 Lighthouse Blvd.', 'Atlantia', 'Pennsylvania', '10222'),
-('009 Endline Circle', 'Faraway', 'Ohio', '19990'),
-('260 Geneva Hwy.', 'Snyder', 'Ohio', '17888'),
-('282 Tulip Way', 'Greenhouse', 'New York', '13357'),
-('985 Sunshine Ave.', 'Positive', 'Indiana', '15929'),
-('171 Staw Lane', 'Brave', 'Pennsylvania', '00829'),
-('200 Jogger Place', 'Toetown', 'West Virginia', '16600'),
-('999 Papyrus Street', 'Jolly', 'New York', '18802'),
-('875 Peanut Hwy.', 'Endor', 'West Virginia', '17070');
+('777 Kindgom Way', '', 'Ararat', 'New York', '13011'),
+('111 Lonestar Street', '', 'Mainville', 'Ohio', '18292'),
+('581 Maisly Ave.', '', 'Gateway', 'Pennsylvania', '16911'),
+('196 Lighthouse Blvd.', '', 'Atlantia', 'Pennsylvania', '10222'),
+('009 Endline Circle', '', 'Faraway', 'Ohio', '19990'),
+('260 Geneva Hwy.', '', 'Snyder', 'Ohio', '17888'),
+('282 Tulip Way', '', 'Greenhouse', 'New York', '13357'),
+('985 Sunshine Ave.', '', 'Positive', 'Indiana', '15929'),
+('171 Staw Lane', '', 'Brave', 'Pennsylvania', '00829'),
+('200 Jogger Place', '', 'Toetown', 'West Virginia', '16600'),
+('999 Papyrus Street', '', 'Jolly', 'New York', '18802'),
+('875 Peanut Hwy.', '', 'Endor', 'West Virginia', '17070');
