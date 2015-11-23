fa8f60fd13202cc3ff15300f2065eb572c1150ba
Girv53 <jacobgirvin@yahoo.com>
2015-11-22 20:57:14 -0500

    Almost finshed with write review form
    need to add code that inserts customer id in table

--- Yellow-Bucket/Yellow-Bucket/WriteMovieReviews.Designer.cs
+++ Yellow-Bucket/Yellow-Bucket/WriteMovieReviews.Designer.cs
@@ -42,8 +42,13 @@
             this.comboBoxMovie = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBoxRating = new System.Windows.Forms.ComboBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.mainMenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -64,7 +69,7 @@
             this.qUITToolStripMenuItem});
             this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
             this.mainMenuStrip.Name = "mainMenuStrip";
-            this.mainMenuStrip.Size = new System.Drawing.Size(118, 444);
+            this.mainMenuStrip.Size = new System.Drawing.Size(118, 579);
             this.mainMenuStrip.TabIndex = 2;
             this.mainMenuStrip.Text = "mainMenuStrip";
             // 
@@ -153,62 +158,126 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(302, 35);
+            this.label1.Location = new System.Drawing.Point(313, 130);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(137, 26);
             this.label1.TabIndex = 3;
             this.label1.Text = "Select Movie";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // comboBoxMovie
             // 
             this.comboBoxMovie.FormattingEnabled = true;
-            this.comboBoxMovie.Location = new System.Drawing.Point(180, 84);
+            this.comboBoxMovie.Location = new System.Drawing.Point(190, 174);
             this.comboBoxMovie.Name = "comboBoxMovie";
             this.comboBoxMovie.Size = new System.Drawing.Size(383, 21);
             this.comboBoxMovie.TabIndex = 6;
+            this.comboBoxMovie.SelectedIndexChanged += new System.EventHandler(this.comboBoxMovie_SelectedIndexChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(302, 207);
+            this.label2.Location = new System.Drawing.Point(312, 301);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(141, 26);
             this.label2.TabIndex = 7;
             this.label2.Text = "Write Review";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(302, 119);
+            this.label3.Location = new System.Drawing.Point(312, 213);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(142, 26);
             this.label3.TabIndex = 8;
             this.label3.Text = "Select Rating";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
-            // comboBox1
+            // comboBoxRating
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(180, 159);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(383, 21);
-            this.comboBox1.TabIndex = 9;
+            this.comboBoxRating.FormattingEnabled = true;
+            this.comboBoxRating.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBoxRating.Location = new System.Drawing.Point(190, 253);
+            this.comboBoxRating.Name = "comboBoxRating";
+            this.comboBoxRating.Size = new System.Drawing.Size(383, 21);
+            this.comboBoxRating.TabIndex = 9;
+            this.comboBoxRating.SelectedIndexChanged += new System.EventHandler(this.comboBoxRating_SelectedIndexChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(180, 244);
+            this.textBox1.Location = new System.Drawing.Point(190, 338);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(383, 20);
             this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(308, 473);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 42);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Post Review";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(312, 380);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 52);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Date Written\r\n\r\n";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(190, 426);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(383, 20);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(312, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 26);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Customer ID\r\n";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(190, 86);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(383, 20);
+            this.textBox3.TabIndex = 15;
             // 
             // WriteMovieReviews
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(658, 444);
+            this.ClientSize = new System.Drawing.Size(658, 579);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.comboBoxRating);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBoxMovie);
@@ -240,7 +309,12 @@
         private System.Windows.Forms.ComboBox comboBoxMovie;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBoxRating;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
